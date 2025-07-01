f34708da539df5f9b3ee5588be717a36a9d05e71
Kartik <thakrekartik92@gmail.com>
2025-07-01 17:08:12 +0530

    make some updation in TodoItem.jsx file

--- react-ts/src/components/TodoItem.tsx
+++ react-ts/src/components/TodoItem.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import classes from "./TodoItem.module.css";
 import { TodosContext } from "../store/Todos-Context";
 
-const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (
-  props
-) => {
+//? we can also use the props directly in the function signature
+//? but using destructuring is more readable and concise
+const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = ({text, onRemoveTodo}) => {
   return (
     <>
-      <li className={classes.item} onClick={props.onRemoveTodo}>
-        {props.text}
+      <li className={classes.item} onClick={onRemoveTodo}>
+        {text}
       </li>
     </>
   );
